fc3cfb7028102c64b8f9eccdba617413425c2933
Lizarndro23 <diegogarzon4723@gmail.com>
2024-12-02 16:25:08 -0500

    se carga el diseño del footer

--- Documentacion/trim03/04_prototipo_funcional/front/src/components/Header.jsx
+++ Documentacion/trim03/04_prototipo_funcional/front/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Header = () => (
   <Navbar className='justify-content-end' bg="dark" variant="dark" expand="lg">
