5f3b40d7a67159bef1d50b3221c4202e73d9cec3
Lizarndro23 <diegogarzon4723@gmail.com>
2024-12-02 16:24:45 -0500

    se carga el diseño del header

--- Documentacion/trim03/04_prototipo_funcional/front/src/components/Footer.jsx
+++ Documentacion/trim03/04_prototipo_funcional/front/src/components/Footer.jsx
@@ -1,9 +1,14 @@
-function Footer(){
-    return(
-        <footer> <img src="../assets/logo-parkeaTec.svg" alt="logo"></img> 
-        </footer>
-        
-    );
-}
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Footer = () => (
+  <footer className="bg-dark text-white py-3 position-fixed bottom-0 w-100">
+    <Container className="text-center">
+      <p>© 2024 Mi Aplicación. Todos los derechos reservados.</p>
+    </Container>
+  </footer>
+);
 
 export default Footer;
