d8b3ba4ec3a0dd528688899fc0c18a696d4f1a65
Lizarndro23 <diegogarzon4723@gmail.com>
2024-12-02 15:29:24 -0500

    Se carga el nuevo header de acuerdo al mockup

--- Documentacion/trim03/04_prototipo_funcional/front/src/components/Header.jsx
+++ Documentacion/trim03/04_prototipo_funcional/front/src/components/Header.jsx
@@ -1,45 +1,39 @@
-function Header(){
-    return(
-        <header class="header">
-        <a href="index.html"><img src="img/logo-parkeaTec.svg" alt="logo" class="logo" /></a>
-        <i class="abrir-menu bi bi-list" id="abrir-menu"></i>
-        <nav class="nav" id="nav">
-            <i class="cerrar-menu bi bi-x" id="cerrar-menu"></i>
-            <ul class="menu-horizontal">
-                <li>
-                    <a class="item-nav" href="#"><i class="fa-solid fa-newspaper"></i>Formulario</a>
-                    <ul class="menu-vertical">
-                        <li><a href="form.html">Llenar Formulario</a></li>
-                        <li><a href="revisarForm.html">Revisar Formulario</a></li>
-                    </ul>
-                </li>
-                <li><a class="item-nav" href="reportes.html"><i class="fas fa-chart-bar"></i>Reportes</a></li>
-                <li>
-                    <a class="item-nav" href=""><i class="bi bi-p-square-fill"></i>Parqueadero</a>
-                    <ul class="menu-vertical">
-                        <li><a href="">Asignar Espacio</a></li>
-                        <li><a href="verEspaciosParqueadero.html">Ver espacios</a></li>
-                        <li><a href="generar-strikes.html">Generar strikes</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a class="item-nav" href="#"><i class="bi bi-person-circle"></i>Perfil</a>
-                    <ul class="menu-vertical">
-                        <li><a href="verqr.html">Ver QR</a></li>
-                        <li><a href="verPerfil.html">Ver perfil</a></li>
-                        <li><a href="realizar-reserva.html">Hacer Reserva</a></li>
-                        <li><a href="misVehiculos.html">Mis vehículos</a></li>
-                        <li><a href="registrarUsuarios.html">Registrar Usuarios</a></li>
-                        <li><a href="">Strikes</a></li>
-                        <li><a href="ver-factura.html">Mis Facturas</a></li>
-                        <li><a href="historial-entrada.html">Historial</a></li>
-                        <li><a href="#modal1">Cerrar Sesión</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    );
-}
+import React from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+
+const Header = () => (
+  <Navbar className='justify-content-end' bg="dark" variant="dark" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">ParkeaTEC</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+            <NavDropdown title='Formulario'>
+                <NavDropdown.Item href='#'>Llenar Formulario</NavDropdown.Item>
+                <NavDropdown.Item href='#'>Revisar Formulario</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#">Reportes</Nav.Link>
+            <NavDropdown title='Parqueadero'>
+              <NavDropdown.Item href='#'>Asignar Espacios</NavDropdown.Item>
+              <NavDropdown.Item href='#'>Ver Espacios</NavDropdown.Item>
+              <NavDropdown.Item href='#'>Generar Strikes</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title='Perfil'>
+              <NavDropdown.Item href='#'>Generar QR</NavDropdown.Item>
+              <NavDropdown.Item href='#'>Ver Perfil</NavDropdown.Item>
+              <NavDropdown.Item href='#'>Hacer Reserva</NavDropdown.Item>
+              <NavDropdown.Item href='#'>Mis Vehiculos</NavDropdown.Item>
+              <NavDropdown.Item href='#'>Registrar Usuarios</NavDropdown.Item>
+              <NavDropdown.Item href='#'>Strikes</NavDropdown.Item>
+              <NavDropdown.Item href='#'>Mis Facturas</NavDropdown.Item>
+              <NavDropdown.Item href='#'>Historial</NavDropdown.Item>
+              <NavDropdown.Item href='#'>Cerrar Sesión</NavDropdown.Item>
+            </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
+
 
 export default Header;
