4eebda4397cd93005cd7ff04aa1b26aae6a425e9
Develpjuan <juanandres78.jg@gmail.com>
2025-05-29 15:22:02 -0500

    Se hace la automatizacion para el registro de usuario

---
+++ Documentacion/trim05/01_Pruebas_software/Parkeatec-Automation/src/main/java/co/com/AutomatizacionParkeatecPage/userinterfaces/Registro.java
@@ -0,0 +1,38 @@
+package co.com.AutomatizacionParkeatecPage.userinterfaces;
+
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
+
+public class Registro extends PageObject {
+
+    public static Target REGISTRO = Target.the("Click enlace registro").located(By.xpath("//*[@id=\"basic-navbar-nav\"]/div/a[1]"));
+
+    public static Target INPUT_NOMBRE = Target.the("Ingreso nombre usuario").located(By.xpath("//*[@id=\"root\"]/div/form/div[1]/div[1]/input"));
+
+    public static Target INPUT_APELLIDO = Target.the("Ingreso apellido usuario").located(By.xpath("//*[@id=\"root\"]/div/form/div[1]/div[2]/input"));
+
+    public static Target INPUT_CORREO = Target.the("Ingreso correo usuario").located(By.xpath("//*[@id=\"root\"]/div/form/div[2]/div[1]/input"));
+
+    public static Target INPUT_PASSWORD = Target.the("Ingreso password usuario").located(By.xpath("//*[@id=\"root\"]/div/form/div[2]/div[2]/input"));
+
+    public static Target INPUT_TELEFONO = Target.the("Ingreso telefono usuario").located(By.xpath("//*[@id=\"root\"]/div/form/div[3]/div[1]/input"));
+
+    public static Target INPUT_DIRECCION = Target.the("Ingreso direccion usuario").located(By.xpath("//*[@id=\"root\"]/div/form/div[3]/div[2]/input"));
+
+    public static Target SELECT_TIPO_DOCUMENTO = Target.the("Seleccion tipo de documento").located(By.xpath("//*[@id=\"root\"]/div/form/div[4]/div[1]/select"));
+
+    public static Target INPUT_NUMERO_DOCUMENTO = Target.the("Ingreso numero de documento usuario").located(By.xpath("//*[@id=\"root\"]/div/form/div[4]/div[2]/input"));
+
+    public static Target INPUT_FOTO = Target.the("Ingreso foto usuario").located(By.cssSelector("input[type=file]"));
+
+    public static Target SELECT_CENTRO = Target.the("Seleccion centro de formacion").located(By.xpath("//*[@id=\"root\"]/div/form/div[5]/div[2]/select"));
+
+    public static Target INPUT_FICHA = Target.the("Ingreso numero ficha usuario").located(By.xpath("//*[@id=\"root\"]/div/form/div[6]/div/input"));
+
+    public static Target BTN_REGISTRO = Target.the("Click boton de registro").located(By.xpath("//*[@id=\"root\"]/div/form/button"));
+
+    public static Target FORM = Target.the("Click boton de registro").located(By.xpath("//*[@id=\"root\"]/div"));
+
+
+}
